79ae8552cc6b473498b99d68bbb4886c9a469aee
Jon Stapleton <stapleton.jon.michael@gmail.com>
2022-03-15 11:36:08 -0400

    added interactivity to CLI

--- index.mjs
+++ index.mjs
@@ -1,19 +1,45 @@
 import fs from 'fs/promises'
 import {write} from 'to-vfile'
 import {renderFile} from './src/renderer.mjs'
+import inquirer from 'inquirer'
 
 main()
 
+async function ask() {
+    const questions = [
+        {
+            name: "readFrom",
+            type: "input",
+            message: "Type the path to the folder containing your modules:",
+            default: "./modules"
+            // TODO: validate format
+        },
+        {
+            name: "writeTo",
+            type: "input",
+            message: "Type the path of the folder you would like to create to contain the newly rendered modules:",
+            default: "./dist"
+            // TODO: check if folder already exists, throw error if it does. Check path format
+        }
+    ]
+    return inquirer.prompt(questions);
+}
+
 async function main() {
+    const {readFrom, writeTo} = await ask()
+    await render(readFrom, writeTo)
+}
+
+async function render(readFrom, writeTo) {
     // Get all the files in ./modules/
-    const files = await getFiles('./modules/')
+    const files = await getFiles(readFrom +'/')
     
     // For each file in ./modules/, create a rendered version in ./dist/
     let madeDirs = []
     for(const file of files) {
         // Create the directory
-        let newDirectory = file.path.replace("./modules/", "")
-        newDirectory = "./dist/"+newDirectory.substring(0, newDirectory.indexOf('/'))+"/"
+        let newDirectory = file.path.replace(readFrom+'/', "")
+        newDirectory = writeTo+"/"+newDirectory.substring(0, newDirectory.indexOf('/'))+"/"
 
         // If the folder doesn't already exist
         if(!madeDirs.includes(newDirectory)) {
