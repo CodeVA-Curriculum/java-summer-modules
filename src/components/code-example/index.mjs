a5f622e1539f1a3828f0028cd49e3ca2a181c721
octo.jon <stapleton.jon.michael@gmail.com>
2022-09-08 14:58:06 -0400

    added tabbed code-example element

--- src/components/code-example/index.mjs
+++ src/components/code-example/index.mjs
@@ -1,26 +1,45 @@
-import parse5 from 'parse5'
-import {readSync} from 'to-vfile'
-import {inspect} from 'unist-util-inspect'
-import {fromParse5} from 'hast-util-from-parse5'
 import {visit, SKIP} from 'unist-util-visit'
-import {toHtml} from 'hast-util-to-html'
+import {h} from 'hastscript'
+
+let count = 0 // Keep track of how many code-example elements we've rendered to avoid id collision in tabs
 
 const CodeExample = (properties, children) => {
-    const file = readSync('./src/components/code-example/index.html')
-    let blob = String(file)
-    // TODO: import course ID from CLI
-    blob = blob.replace("{{ imageURL }}", `https://canvas.instructure.com/courses/13038/files/${properties.src}/preview`)
-    blob = blob.replace("{{ altText }}", properties.alt)
-    
-    // Convert children to plaintext
-    const child = toHtml(children)
-    blob=blob.replace('{{ children }}', child)
+    // Clean input
+    if(typeof(properties.tabs) == typeof('str')) {
+        properties.tabs = true;
+    } else {
+        properties.tabs = false;
+    }
 
-    const p5ast = parse5.parse(blob, {sourceCodeLocationInfo: true})
-    const hast = fromParse5(p5ast, file)
+    // Construct template hast
+    const hast = h('article.code-example', [
+        h('.enhanceable_content.tabs.tab-content', [...children])
+    ])
 
+    let tabElements = []
     visit(hast, 'element', function (node, index, parent) {
-        if (['body'].includes(node.tagName)) {
+        // Find name, transform it into an <li> and add it to the `tabElements` list, remove from tree
+        if(node.tagName == 'name' && properties.tabs) {
+            // Transform into link, save node for later in `tabElements` list
+            node.tagName = 'a'
+            node.properties.href = `#fragment-${count}${tabElements.length+1}`
+            tabElements.push(h('li', [node]))
+
+            // Remove all the children preceding the `name` element, and add them back wrapped in the appropriate div
+            const children = parent.children.splice(tabElements.length, index+1)
+            const div = h(`#fragment-${count}${tabElements.length}`, [
+                ...children
+            ])
+            parent.children.splice(tabElements.length, 0, div)
+
+            // Remove `name` node from tree
+            parent.children.splice(index, 1)
+            return [SKIP, index]
+        }
+        
+
+        // take `hast` tree out of "body" wrapper
+        if (['body', 'head', 'html'].includes(node.tagName)) {
             parent.children.splice(index, 1, ...node.children)
             // Do not traverse `node`, continue at the node *now* at `index`.
             return [SKIP, index]
@@ -28,6 +47,29 @@ const CodeExample = (properties, children) => {
             return
         }
     })
+
+    // If we need to add tabs...
+    if(properties.tabs) {
+        // Construct `ul` element for tabs
+        const ul = h('ul.tabs-list', [
+            ...tabElements
+        ])
+        // Add `ul` to hast
+        hast.children[0].children.splice(0, 0, ul)
+    }
+    // Add image, if appropriate
+    let addImage = false
+    if(typeof(properties.src) == typeof('str')) {
+        addImage = true
+    }
+    if(addImage) {
+        const src = properties.src.includes('http') ? properties.src : `https://canvas.instructure.com/courses/13038/files/${properties.src}/preview`
+        const img = h('.code-example-image-wrapper',[
+            h('img.code-example-image', {src:src, alt: properties.alt})
+        ])
+        hast.children.splice(hast.children.length, 0, img)
+    }
+    
 
     // add styles to 'pre' tag
     // visit(hast, 'element', function (node, index, parent) {
@@ -37,7 +79,7 @@ const CodeExample = (properties, children) => {
     //         node.properties.style = existingStyles+"min-width: 60%; max-height: 400px; overflow: scroll; overflow-x: hidden; -webkit-column-break-inside: avoid; page-break-inside: avoid; break-inside: avoid;"
     //     }
     // })
-    
+    count++
     return hast
 }
     
---
+++ src/tests/code-example/tabs.md
@@ -0,0 +1,29 @@
+:::code-example{src="https://placekitten.com/400/400" alt="A kitten" tabs}
+::name[Main]
+```
+The main tab
+```
+
+::name[Another]
+```
+Another tab
+```
+:::
+
+:::code-example{tabs}
+::name[Main]
+```
+No image
+```
+
+::name[Another]
+```
+No image another
+```
+:::
+
+:::code-example{src="https://placekitten.com/400/400" alt="A kitten"}
+```
+No tabs
+```
+:::
